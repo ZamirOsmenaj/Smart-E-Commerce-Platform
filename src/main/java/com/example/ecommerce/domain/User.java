c7da38dd902eb1bdea3721a8eb0b43715d9b888d
Zamir <zamir.osmenaj28@gmail.com>
2025-08-30 17:45:16 +0300

    feat: create user domain model and repository (JPA)

---
+++ src/main/java/com/example/ecommerce/domain/User.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP") // Optional
+    private Instant createdAt = Instant.now();
+}
